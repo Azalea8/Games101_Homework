25c8d005f0c8d4950158f53f50b3d99e12a47e36
Azalea <2574292235@qq.com>
2023-10-18 10:22:54 +0800

    排版修正

--- Assignment1/代码框架/main.cpp
+++ Assignment1/代码框架/main.cpp
@@ -12,8 +12,10 @@ Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
     Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
 
     Eigen::Matrix4f translate;
-    translate << 1, 0, 0, -eye_pos[0], 0, 1, 0, -eye_pos[1], 0, 0, 1,
-        -eye_pos[2], 0, 0, 0, 1;
+    translate << 1, 0, 0, -eye_pos[0],
+                 0, 1, 0, -eye_pos[1],
+                 0, 0, 1, -eye_pos[2],
+                 0, 0, 0, 1;
 
     view = translate * view;
 
@@ -58,7 +60,7 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
         0, 0, -zNear - zFar, zNear * -zFar,
         0, 0, 1, 0;
 
-    float halve = (eye_fov/2) * MY_PI/180;
+    float halve = (eye_fov/2.0) * MY_PI/180;
     float top = tan(halve) * zNear;
     float bottom = -top;
     float right = top * aspect_ratio;

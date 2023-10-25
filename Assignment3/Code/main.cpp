f3ed162e1b33766c14fbe61b91681a42df202252
Azalea <2574292235@qq.com>
2023-10-25 19:09:41 +0800

    init

--- Assignment3/Code/main.cpp
+++ Assignment3/Code/main.cpp
@@ -13,9 +13,9 @@ Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos) {
 
     Eigen::Matrix4f translate;
     translate << 1, 0, 0, -eye_pos[0],
-            0, 1, 0, -eye_pos[1],
-            0, 0, 1, -eye_pos[2],
-            0, 0, 0, 1;
+                0, 1, 0, -eye_pos[1],
+                0, 0, 1, -eye_pos[2],
+                0, 0, 0, 1;
 
     view = translate * view;
 
@@ -38,9 +38,9 @@ Eigen::Matrix4f get_model_matrix(float angle) {
 
     Eigen::Matrix4f translate;
     translate << 1, 0, 0, 0,
-            0, 1, 0, 0,
-            0, 0, 1, 0,
-            0, 0, 0, 1;
+                0, 1, 0, 0,
+                0, 0, 1, 0,
+                0, 0, 0, 1;
 
     return translate * rotation * scale;
 }

0ef54dffb88d50bc7879bf14fa5484eff94c43de
Azalea <2574292235@qq.com>
2023-12-07 10:33:00 +0800

    作业4完成

--- Assignment4/code/main.cpp
+++ Assignment4/code/main.cpp
@@ -4,16 +4,19 @@
 
 std::vector<cv::Point2f> control_points;
 
+// 贝塞尔曲线的控制点数量，默认为 4，即 3阶贝塞尔曲线
+#define NUM 4
+
 void mouse_handler(int event, int x, int y, int flags, void *userdata) 
 {
-    if (event == cv::EVENT_LBUTTONDOWN && control_points.size() < 4) 
+    if (event == cv::EVENT_LBUTTONDOWN && control_points.size() < NUM)
     {
-        std::cout << "Left button of the mouse is clicked - position (" << x << ", "
-        << y << ")" << '\n';
+        std::cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << '\n';
         control_points.emplace_back(x, y);
     }     
 }
 
+// 显式调用，没有递归
 void naive_bezier(const std::vector<cv::Point2f> &points, cv::Mat &window) 
 {
     auto &p_0 = points[0];
@@ -26,22 +29,52 @@ void naive_bezier(const std::vector<cv::Point2f> &points, cv::Mat &window)
         auto point = std::pow(1 - t, 3) * p_0 + 3 * t * std::pow(1 - t, 2) * p_1 +
                  3 * std::pow(t, 2) * (1 - t) * p_2 + std::pow(t, 3) * p_3;
 
+        // 曲线颜色设置为红色，OpenCV为 BGR
         window.at<cv::Vec3b>(point.y, point.x)[2] = 255;
     }
 }
 
-cv::Point2f recursive_bezier(const std::vector<cv::Point2f> &control_points, float t) 
+// 贝塞尔曲线的基本公式
+cv::Point2f lerp_v2f(const cv::Point2f& a, const cv::Point2f& b, float t)
 {
-    // TODO: Implement de Casteljau's algorithm
-    return cv::Point2f();
-
+    return (1 - t) * a + t * b;
 }
 
+// 递归
+cv::Point2f recursive_bezier(const std::vector<cv::Point2f>& control_points, float t)
+{
+    // TODO: Implement de Casteljau's algorithm
+    // 递归出口，此时为贝塞尔曲线上的点
+    if (control_points.size() == 1)
+    {
+        return control_points[0];
+    }
+
+    // 新建一个数组，储存新的控制点，数量每次会减少 1，减少到 1时即为出口
+    std::vector<cv::Point2f> lerp_points;
+    // 遍历原控制点数组，将通过 lerp_v2f（最基本的公式）新形成的控制点依次填入新的数组
+    for (size_t i = 1;i < control_points.size();i++)
+    {
+        lerp_points.push_back(lerp_v2f(control_points[i - 1], control_points[i], t));
+    }
+    // 新数组继续递归
+    return recursive_bezier(lerp_points, t);
+}
+
+// 遍历参数 t
 void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window) 
 {
     // TODO: Iterate through all t = 0 to t = 1 with small steps, and call de Casteljau's 
     // recursive Bezier algorithm.
+    for (double t = 0.0;t <= 1.0; t += 0.001)
+    {
+        // 对于每一个 t，求它对应的贝塞尔曲线的点，这里是通过最基本的公式 (1 - t) * a + t * b，递归求得，
+        // 实际上也可以直接用 n阶展开式，直接求得，就像 naive_bezier函数那样
+        auto point = recursive_bezier(control_points, t);
 
+        // 坐标系转换，吐槽一句，opencv鼠标点击 和 opencv储存图像的坐标系不一样
+        window.at<cv::Vec3b>(point.y, point.x)[2] = 255;
+    }
 }
 
 int main() 
@@ -60,10 +93,10 @@ int main()
             cv::circle(window, point, 3, {255, 255, 255}, 3);
         }
 
-        if (control_points.size() == 4) 
+        if (control_points.size() == NUM)
         {
-            naive_bezier(control_points, window);
-            //   bezier(control_points, window);
+            // naive_bezier(control_points, window);
+            bezier(control_points, window);
 
             cv::imshow("Bezier Curve", window);
             cv::imwrite("my_bezier_curve.png", window);
